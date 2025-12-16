c1d24b2336e0a3c37abbfedf069e6bb5db394257
pujandhakal <dhakalpujan72@gmail.com>
2025-12-16 21:14:35 +0545

    Initializing Trending DealsScreen

---
+++ lib/pages/user/trending_deals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrendingDealsScreen extends StatefulWidget {
+  const TrendingDealsScreen({super.key});
+
+  @override
+  State<TrendingDealsScreen> createState() => _TrendingDealsScreenState();
+}
+
+class _TrendingDealsScreenState extends State<TrendingDealsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
