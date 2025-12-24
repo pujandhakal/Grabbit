e320b8c55be42a7736b7f2f9baa8917657686dfe
pujandhakal <dhakalpujan72@gmail.com>
2025-12-24 22:36:53 +0545

    Make Image section in groupbuy card

--- lib/util/join_groupbuy_card.dart
+++ lib/util/join_groupbuy_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class JoinGroupbuyCard extends StatefulWidget {
   const JoinGroupbuyCard({super.key});
@@ -45,16 +46,33 @@ class _JoinGroupbuyCardState extends State<JoinGroupbuyCard> {
                   "https://picsum.photos/400/200",
                 ),
               ),
+              Positioned.fill(
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.white.withValues(alpha: 0.5),
+                        Colors.transparent,
+                      ],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      stops: [0.1, 0.7],
+                    ),
+                  ),
+                ),
+              ),
               Positioned(
                 top: 16,
                 right: 16,
                 child: Column(
                   children: [
                     Container(
+                      width: 90,
                       padding: EdgeInsets.symmetric(
                         vertical: 6,
                         horizontal: 12,
                       ),
+                      margin: EdgeInsets.only(bottom: 4),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(8),
                         gradient: LinearGradient(
@@ -68,6 +86,7 @@ class _JoinGroupbuyCardState extends State<JoinGroupbuyCard> {
                         ),
                       ),
                       child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Icon(
                             Icons.electric_bolt_sharp,
@@ -81,14 +100,146 @@ class _JoinGroupbuyCardState extends State<JoinGroupbuyCard> {
                             "21% off",
                             style: TextStyle(
                               color: Colors.white,
+                              fontSize: 10,
                             ),
                           ),
                         ],
                       ),
-                    )
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 6,
+                        horizontal: 12,
+                      ),
+                      margin: EdgeInsets.only(bottom: 4),
+                      width: 90,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xffAD46FF),
+                            Color(0xffF6339A),
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.local_fire_department_outlined,
+                            size: 12,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Text(
+                            "Trending",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 6,
+                        horizontal: 12,
+                      ),
+                      width: 90,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xffFDC700),
+                            Color(0xffFF6900),
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/star_icon.svg',
+                            height: 12,
+                            width: 12,
+                            colorFilter: ColorFilter.mode(
+                              Colors.white,
+                              BlendMode.srcIn,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Text(
+                            "Hot Deal",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 10,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
+              Positioned(
+                bottom: 16,
+                left: 16,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.black.withValues(alpha: 0.8),
+                        Colors.black.withValues(alpha: 0.6),
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(color: Colors.white10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(25, 0, 0, 0),
+                        offset: Offset(0, 20),
+                        blurRadius: 25,
+                        spreadRadius: -5,
+                      ),
+                      BoxShadow(
+                        color: Color.fromARGB(25, 0, 0, 0),
+                        offset: Offset(0, 8),
+                        blurRadius: 10,
+                        spreadRadius: -6,
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.schedule_outlined,
+                        color: Color(0xffFFB86A),
+                        size: 16,
+                      ),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        "Ends in 3h",
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
 
